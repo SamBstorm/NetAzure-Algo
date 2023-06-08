c7968a2ac09db987ffa0f2f44148ba5facb9adf3
Samuel Legrain <samuel.legrain@bstorm.be>
2023-06-08 16:12:53 +0200

    Ajout Constante Demo

--- Demo-intro/Program.cs
+++ Demo-intro/Program.cs
@@ -8,6 +8,9 @@
             message = "Hello les .net azure!";
             //message = 42;     //Attention, ne pas sauvegarder un type incompatible
             Console.WriteLine(message);
+
+            const int ANNEE = 2023;
+            //ANNEE = 2024;     //Attention, une constante ne peut être modifiée
         }
     }
 }
