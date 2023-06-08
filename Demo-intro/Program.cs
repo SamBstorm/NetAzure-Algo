09b4dc834afb5af1d0d2ae5b617f630e7165a7cf
Samuel Legrain <samuel.legrain@bstorm.be>
2023-06-08 15:55:03 +0200

    Ajoutez des fichiers projet.

---
+++ Demo-intro/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Demo_intro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message;
+            message = "Hello les .net azure!";
+            //message = 42;     //Attention, ne pas sauvegarder un type incompatible
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ Exo1-Inversion/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exo1_Inversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 7;
+            int b = 5;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            int c = a;
+            a = b;
+            b = c;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
